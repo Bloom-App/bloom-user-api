9bca6d52bba6cd7fcdd516d676136516d8b0d432
Alban GUILLET <albanguillet98@gmail.com>
2020-04-05 16:17:15 +0200

    Initial commit

---
+++ src/main/kotlin/fr/albanguillet/bloomuserapi/BloomUserApiApplication.kt
@@ -0,0 +1,11 @@
+package fr.albanguillet.bloomuserapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BloomUserApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BloomUserApiApplication>(*args)
+}
---
+++ src/test/kotlin/fr/albanguillet/bloomuserapi/BloomUserApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package fr.albanguillet.bloomuserapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BloomUserApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
